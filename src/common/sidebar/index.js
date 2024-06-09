ba9336fbe40a52db6d81927ae19b1c66b8e0f397
mitisha14 <mitishadankhara003@gmail.com>
2024-06-09 11:20:04 +0530

    Project Setup

---
+++ src/common/sidebar/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./sidebar.scss";
+export default function Sidebar() {
+  return <div>Sidebar</div>;
+}
---
+++ src/component/herobanner/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./herobanner.scss";
+export default function Herobanner() {
+  return <div>Herobanner</div>;
+}
